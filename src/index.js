4707359071d927e885db32e7c34c57482e2feb66
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-14 11:09:26 +0800

    Add imported function from add-project js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './reset.css';
 import './common.css';
+import './sidebar.css';
+import './main-content.css';
 
 import { sidebarModalFunction } from './sidebar-control';
+import { NewProjectFunction } from './add-project';
 
 sidebarModalFunction();
+NewProjectFunction();
