a3b03e7e15c67bdc65b3847d9300500178a217e4
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-16 21:51:07 +0800

    Add buttons for viewing, editing, and deleting of tasks

--- src/landing.js
+++ src/landing.js
@@ -1,3 +1,7 @@
+import deleteSvg from './del.svg';
+import editSvg from './edit.svg';
+import viewSvg from './view.svg';
+
 const landingProject = (() => {
     const sampleProject = {
         title: "Project Sample",
@@ -11,17 +15,16 @@ const landingProject = (() => {
             },
             {
                 title: "Task sample two",
-                desc: "Task description two",
+                desc: "Task description two Task description two Task description two Task description two",
                 due: "01-01-2111",
                 prio: "High"
             }
         ]
     }
 
-    const main = document.querySelector("main");
-
-    const projectTitle = document.createElement("h1");
-    const projectDescription = document.createElement("h2");
+    const main = document.querySelector("main"),
+          projectTitle = document.createElement("h1"),
+          projectDescription = document.createElement("h2");
 
     projectTitle.textContent = sampleProject.title;
     projectDescription.textContent = sampleProject.desc;
@@ -32,22 +35,47 @@ const landingProject = (() => {
     );
 
     for (let tasks of sampleProject.tasks) {
-        const taskContainer = document.createElement("div");
-        const taskTitle = document.createElement("h3");
-        const taskDescription = document.createElement("p");
-        const taskDue = document.createElement("p");
-        const taskPriority = document.createElement("p");
+      const taskContainer = document.createElement("div"),
+            taskTitle = document.createElement("h3"),
+            taskDescription = document.createElement("p"),
+            taskDue = document.createElement("p"),
+            taskPriority = document.createElement("p"),
+            taskDoneBtn = document.createElement("button"),
+            taskViewBtn = document.createElement("button"),
+            taskEditBtn = document.createElement("button"),
+            taskDeleteBtn = document.createElement("button"),
+            viewIcon = new Image(),
+            editIcon = new Image(),
+            deleteIcon = new Image();
 
         taskTitle.textContent = tasks.title;
         taskDescription.textContent = "Description: " + tasks.desc;
         taskDue.textContent = "Due Date: " + tasks.due;
         taskPriority.textContent = "Priority: " + tasks.prio;
+        
+        taskDoneBtn.setAttribute("class", "mark-done");
+        taskDoneBtn.textContent = "Mark as Done";
+
+        taskViewBtn.setAttribute("class", "view-task");
+        viewIcon.src = viewSvg;
+        taskEditBtn.setAttribute("class", "edit-task");
+        editIcon.src = editSvg;
+        taskDeleteBtn.setAttribute("class", "delete-task");
+        deleteIcon.src = deleteSvg;
+
+        taskViewBtn.append(viewIcon);
+        taskEditBtn.append(editIcon);
+        taskDeleteBtn.append(deleteIcon);
 
         taskContainer.append(
             taskTitle,
             taskDescription,
             taskDue,
-            taskPriority
+            taskPriority,
+            taskDoneBtn,
+            taskViewBtn,
+            taskEditBtn,
+            taskDeleteBtn
         );
 
         main.append(taskContainer)
