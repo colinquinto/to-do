e4a059734194d16aa734e3fa070f869160eaecfc
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-17 18:41:26 +0800

    Handle imported functions from newly created js file

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import './main-content.css';
 import { sidebarModalFunction } from './sidebar-control';
 import { NewProjectFunction } from './add-project';
 import { landingProject } from './landing';
+import { renderProjects } from './display-projects';
+
 
 sidebarModalFunction();
 NewProjectFunction();
 landingProject;
+renderProjects();
 
