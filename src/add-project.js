b663555e046373ad4c333da50c771efdf6942390
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-17 18:40:07 +0800

    Add input value validation if it is a duplicate, store it to localstorage for later use, then use imported function to render it on the sidebar

--- src/add-project.js
+++ src/add-project.js
@@ -1,8 +1,14 @@
+
+import { createProjectElements } from './display-projects';
+
 const NewProjectFunction = () => {
     const addProjectDialog = document.querySelector(".add-project-dialog");
     const newProjectBtn = document.querySelector(".add-project");
     const cancel = document.querySelector(".cancel-project");
+    const confirm = document.querySelector(".confirm-project");
     const inputTitle = document.querySelector(".proj-title-input");
+
+    
 
     newProjectBtn.addEventListener("click", () => addProjectDialog.show())
 
@@ -11,6 +17,16 @@ const NewProjectFunction = () => {
         inputTitle.value = "";
     })
 
+    confirm.addEventListener("click", () => {
+        if (inputTitle.value in localStorage) {
+            alert("THIS PROJECT ALREADY EXISTS!")
+        }
+        else {
+            localStorage.setItem(inputTitle.value, JSON.stringify({}));
+            createProjectElements(inputTitle.value);
+            inputTitle.value = "";
+        }
+    })
 }
 
 export { NewProjectFunction }
