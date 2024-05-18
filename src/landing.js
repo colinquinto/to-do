ebcfeed4a8dae94443e9ff6a044f04cfe90716ba
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-18 14:18:58 +0800

    Add placeholder project to localstorage on page load

--- src/landing.js
+++ src/landing.js
@@ -1,11 +1,8 @@
-import deleteSvg from './del.svg';
-import editSvg from './edit.svg';
-import viewSvg from './view.svg';
+import { renderTasks } from "./display-tasks";
 
 const landingProject = (() => {
     const sampleProject = {
         title: "Project Sample",
-        desc: "Tasks",
         tasks: [
             {
                 title: "Task sample",
@@ -21,69 +18,9 @@ const landingProject = (() => {
             }
         ]
     }
-    localStorage.setItem("Project Sample", JSON.stringify(sampleProject))
-    const main = document.querySelector("main"),
-          projectTitle = document.createElement("h1"),
-          projectDescription = document.createElement("h2");
-
-    projectTitle.textContent = sampleProject.title;
-    projectDescription.textContent = sampleProject.desc;
-
-    main.append(
-        projectTitle,
-        projectDescription,
-    );
-
-    for (let tasks of sampleProject.tasks) {
-      const taskContainer = document.createElement("div"),
-            taskTitle = document.createElement("h3"),
-            taskDescription = document.createElement("p"),
-            taskDue = document.createElement("p"),
-            taskPriority = document.createElement("p"),
-            taskDoneBtn = document.createElement("button"),
-            taskViewBtn = document.createElement("button"),
-            taskEditBtn = document.createElement("button"),
-            taskDeleteBtn = document.createElement("button"),
-            viewIcon = new Image(),
-            editIcon = new Image(),
-            deleteIcon = new Image();
-
-        taskTitle.textContent = tasks.title;
-        taskDescription.textContent = "Description: " + tasks.desc;
-        taskDue.textContent = "Due Date: " + tasks.due;
-        taskPriority.textContent = "Priority: " + tasks.prio;
-        taskViewBtn.textContent = "View";
-        taskEditBtn.textContent = "Edit";
-        taskDeleteBtn.textContent = "Delete"
-        
-        taskDoneBtn.setAttribute("class", "mark-done");
-        taskDoneBtn.textContent = "Mark as Done";
-
-        taskViewBtn.setAttribute("class", "view-task");
-        viewIcon.src = viewSvg;
-        taskEditBtn.setAttribute("class", "edit-task");
-        editIcon.src = editSvg;
-        taskDeleteBtn.setAttribute("class", "delete-task");
-        deleteIcon.src = deleteSvg;
-
-        taskViewBtn.append(viewIcon);
-        taskEditBtn.append(editIcon);
-        taskDeleteBtn.append(deleteIcon);
-
-        taskContainer.append(
-            taskTitle,
-            taskDescription,
-            taskDue,
-            taskPriority,
-            taskDoneBtn,
-            taskViewBtn,
-            taskEditBtn,
-            taskDeleteBtn
-        );
-
-        main.append(taskContainer)
-    }
-
+    localStorage.setItem("Project Sample", JSON.stringify(sampleProject));
+    const getProject = JSON.parse(localStorage.getItem("Project Sample"));
+    renderTasks(getProject);
 })();
 
 export { landingProject };
