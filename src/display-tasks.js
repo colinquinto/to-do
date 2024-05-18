500381ab08dea3524c655d5342e77122757e192d
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-18 20:33:13 +0800

    Edit code spacing

--- src/display-tasks.js
+++ src/display-tasks.js
@@ -1,80 +1,98 @@
 import deleteSvg from './del.svg';
 import editSvg from './edit.svg';
 import viewSvg from './view.svg';
+import { getNewTask } from './add-task';
 
-const renderTasks = (taskPara) => {
-    const main = document.querySelector("main");
-    main.innerHTML = "";
-    const projectTitle = document.createElement("h1");
-    const taskInitial = document.createElement("h2");
-    const newTaskDiv = document.createElement("div");
-    const createNewTask = document.createElement("button");
+class taskComponents {
+    main = document.querySelector("main");
 
-    projectTitle.textContent = taskPara.title;
-    taskInitial.textContent = "Tasks";
-
-    newTaskDiv.setAttribute("class", "new-task")
-    createNewTask.textContent = "Add Task";
-
-    newTaskDiv.append(createNewTask);
-
-    main.append(
-        projectTitle,
-        taskInitial,
-        newTaskDiv
-    );
-    if (taskPara.tasks[0].title === ""){
-        return
+    constructor(project, task){
+        this.project = project;
+        this.task = task;
     }
-    else {
-        for (let tasks of taskPara.tasks) {
-      const taskContainer = document.createElement("div");
-      const taskTitle = document.createElement("h3");
-      const taskDescription = document.createElement("p");
-      const taskDue = document.createElement("p");
-      const taskPriority = document.createElement("p");
-      const taskDoneBtn = document.createElement("button");
-      const taskViewBtn = document.createElement("button");
-      const taskEditBtn = document.createElement("button");
-      const taskDeleteBtn = document.createElement("button");
-      const viewIcon = new Image();
-      const editIcon = new Image();
-      const deleteIcon = new Image();
 
-        taskTitle.textContent = tasks.title;
-        taskDescription.textContent = "Description: " + tasks.desc;
-        taskDue.textContent = "Due Date: " + tasks.due;
-        taskPriority.textContent = "Priority: " + tasks.prio;
-        taskViewBtn.textContent = "View";
-        taskEditBtn.textContent = "Edit";
-        taskDeleteBtn.textContent = "Delete"
-        
-        taskDoneBtn.setAttribute("class", "mark-done");
-        taskDoneBtn.textContent = "Mark as Done";
-
-        taskViewBtn.setAttribute("class", "view-task");
-        viewIcon.src = viewSvg;
-        taskEditBtn.setAttribute("class", "edit-task");
-        editIcon.src = editSvg;
-        taskDeleteBtn.setAttribute("class", "delete-task");
-        deleteIcon.src = deleteSvg;
-
-        taskViewBtn.append(viewIcon);
-        taskEditBtn.append(editIcon);
-        taskDeleteBtn.append(deleteIcon);
-
-        taskContainer.append(
-            taskTitle,
-            taskDescription,
-            taskDue,
-            taskPriority,
-            taskDoneBtn,
-            taskViewBtn,
-            taskEditBtn,
-            taskDeleteBtn
+    renderTitle = () => {
+        this.main.innerHTML = "";
+        const projectTitle = document.createElement("h1");
+        const taskInitial = document.createElement("h2");
+        const newTaskDiv = document.createElement("div");
+        const createNewTask = document.createElement("button");
+    
+        projectTitle.textContent = this.project;
+        taskInitial.textContent = "Tasks";
+    
+        newTaskDiv.setAttribute("class", "new-task");
+        createNewTask.setAttribute("class", "new-task-button");
+        createNewTask.textContent = "Add Task";
+    
+        newTaskDiv.append(createNewTask);
+    
+        this.main.append(
+            projectTitle,
+            taskInitial,
+            newTaskDiv
         );
-        main.append(taskContainer)
-    }} 
+        getNewTask();
+    }
+
+    renderTasks = () => {
+        if (this.task[0].title === ""){
+            return
+        }
+        else {
+            for (let tasks of this.task) {
+          const taskContainer = document.createElement("div");
+          const taskTitle = document.createElement("h3");
+          const taskDescription = document.createElement("p");
+          const taskDue = document.createElement("p");
+          const taskPriority = document.createElement("p");
+
+          const taskDoneBtn = document.createElement("button");
+          const taskViewBtn = document.createElement("button");
+          const taskEditBtn = document.createElement("button");
+          const taskDeleteBtn = document.createElement("button");
+          
+          const viewIcon = new Image();
+          const editIcon = new Image();
+          const deleteIcon = new Image();
+    
+            taskTitle.textContent = tasks.title;
+            taskDescription.textContent = "Description: " + tasks.desc;
+            taskDue.textContent = "Due Date: " + tasks.due;
+            taskPriority.textContent = "Priority: " + tasks.prio;
+            taskViewBtn.textContent = "View";
+            taskEditBtn.textContent = "Edit";
+            taskDeleteBtn.textContent = "Delete"
+            
+            taskDoneBtn.setAttribute("class", "mark-done");
+            taskDoneBtn.textContent = "Mark as Done";
+    
+            taskViewBtn.setAttribute("class", "view-task");
+            viewIcon.src = viewSvg;
+
+            taskEditBtn.setAttribute("class", "edit-task");
+            editIcon.src = editSvg;
+
+            taskDeleteBtn.setAttribute("class", "delete-task");
+            deleteIcon.src = deleteSvg;
+    
+            taskViewBtn.append(viewIcon);
+            taskEditBtn.append(editIcon);
+            taskDeleteBtn.append(deleteIcon);
+    
+            taskContainer.append(
+                taskTitle,
+                taskDescription,
+                taskDue,
+                taskPriority,
+                taskDoneBtn,
+                taskViewBtn,
+                taskEditBtn,
+                taskDeleteBtn
+            );
+            this.main.append(taskContainer)
+        }} 
+    }
 }
 
-export { renderTasks }
+export { taskComponents }
