8caf3c1a928bbd7dd8883adae444a07ea62a2962
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-18 20:35:14 +0800

    Reduce placholder task of sample project

--- src/landing.js
+++ src/landing.js
@@ -1,26 +1,22 @@
-import { renderTasks } from "./display-tasks";
+import { taskComponents } from "./display-tasks";
 
 const landingProject = (() => {
     const sampleProject = {
-        title: "Project Sample",
+        title: "-Project Sample",
         tasks: [
             {
                 title: "Task sample",
                 desc: "Task description",
                 due: "01-01-2111",
                 prio: "High"
-            },
-            {
-                title: "Task sample two",
-                desc: "Task description two Task description two Task description two Task description two",
-                due: "01-01-2111",
-                prio: "High"
             }
         ]
     }
-    localStorage.setItem("Project Sample", JSON.stringify(sampleProject));
-    const getProject = JSON.parse(localStorage.getItem("Project Sample"));
-    renderTasks(getProject);
+    localStorage.setItem("-Project Sample", JSON.stringify(sampleProject));
+    const getProject = JSON.parse(localStorage.getItem("-Project Sample"));
+    const renderSample = new taskComponents(getProject.title, getProject.tasks);
+    renderSample.renderTitle();
+    renderSample.renderTasks();
 })();
 
 export { landingProject };
