7f99abb6e95e9d6aecce292e5fd498260abaeed5
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-18 14:18:34 +0800

    Update renamed functions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import './common.css';
 import './sidebar.css';
 import './main-content.css';
 
-import { sidebarModalFunction } from './sidebar-control';
+import { sidebarModalFunction, sidebarProjectsEvent } from './sidebar-control';
 import { NewProjectFunction } from './add-project';
 import { landingProject } from './landing';
-import { renderProjects } from './display-projects';
+import { renderProjectsToSidebar } from './display-projects';
 
 
 sidebarModalFunction();
 NewProjectFunction();
 landingProject;
-renderProjects();
-
+renderProjectsToSidebar();
+sidebarProjectsEvent();
+// localStorage.clear()
