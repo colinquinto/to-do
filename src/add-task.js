491c1ce46753ccce551214e17933c0692a7ca8b0
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-19 17:59:27 +0800

    Add functions that will handle the modal, click events, and storing input data to localstorage

--- src/add-task.js
+++ src/add-task.js
@@ -1,17 +1,73 @@
-const getNewTask = () => {
-    const newTaskButton = document.querySelector(".new-task-button");
-    const taskDialog = document.querySelector(".task-modal");
-    const closeTaskDialog = document.querySelector(".task-modal-buttons > #close")
+import { taskComponents } from "./display-tasks";
 
-    newTaskButton.addEventListener("click", () => {
-        const getTitle = document.querySelector("main > h1");
-        const getData = JSON.parse(localStorage.getItem(getTitle.textContent))
-        taskDialog.showModal();
-    })
+const taskModalFunc = () => {
+    const taskModal = document.querySelector(".task-modal");
+    const newTaskBtn = document.querySelector(".new-task-button");
+    const form = document.querySelector(".task-modal > form");
+    const cancelNewTask = document.querySelector("#close");
+    const submitTask = document.querySelector("#submit");
 
-    closeTaskDialog.addEventListener("click", () => {
-        taskDialog.close();
-    })
+    if (newTaskBtn){newTaskBtn.addEventListener("click", () => {
+        taskModal.showModal();
+    })}
+
+    cancelNewTask.addEventListener("click", cancelEvent);
+    submitTask.addEventListener("click", submitEvent);
+
+    return {
+        taskModal,
+        form
+    }
+    
+};
+
+const submitEvent = (evt) => {
+    if (taskModalFunc().form.checkValidity()) {
+        evt.preventDefault();
+        const projectName = document.querySelector("main > h1");
+        const projectData = JSON.parse(localStorage.getItem(projectName.textContent))
+
+        const taskArr = [];
+
+        for (let i = 0; i < 3; i++) {
+            taskArr.push(taskModalFunc().form.elements[i].value)
+        }
+
+        taskArr.push(taskModalFunc().form.querySelector("input[type='radio']:checked").value)
+        taskModalFunc().form.reset();
+        taskModalFunc().taskModal.close();
+
+        submitToStorage(projectName.textContent, taskArr[0], taskArr[1], taskArr[2], taskArr[3])
+
+    }
 }
 
-export { getNewTask };
+const submitToStorage = (proj, getTitle, getDesc, getDue, getPrio) => {
+    const projObj = {
+        title: getTitle,
+        desc: getDesc,
+        due: getDue,
+        prio: getPrio
+    }
+
+    const getProj = JSON.parse(localStorage.getItem(proj))
+    getProj.tasks.push(projObj)
+
+    localStorage.setItem(proj, JSON.stringify(getProj))
+    const main = document.querySelector("main");
+    main.innerHTML="";
+    const getProject = JSON.parse(localStorage.getItem(proj));
+    const newProject = new taskComponents(getProject.title, getProject.tasks);
+    newProject.renderTitle();
+    newProject.renderTasks();
+    taskModalFunc();
+}
+
+const cancelEvent = (e) => {
+    e.preventDefault();
+    console.log("Cancelled")
+    taskModalFunc().form.reset();
+    taskModalFunc().taskModal.close();
+}
+
+export { taskModalFunc };
