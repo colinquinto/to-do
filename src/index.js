2cfea61f347d58d3ad27edff7b333681a2fc4664
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-19 18:01:14 +0800

    Import task modal function

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import './common.css';
 import './sidebar.css';
 import './main-content.css';
 
+import { landingProject } from './landing';
 import { sidebarModalFunction, sidebarProjectsEvent } from './sidebar-control';
 import { NewProjectFunction } from './add-project';
-import { landingProject } from './landing';
 import { renderProjectsToSidebar } from './display-projects';
+import { taskModalFunc } from './add-task';
 
 sidebarModalFunction();
 NewProjectFunction();
 renderProjectsToSidebar();
 sidebarProjectsEvent();
-landingProject;
+taskModalFunc();
 
 // localStorage.clear()
