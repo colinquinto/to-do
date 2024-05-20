67fac021e438f6cbc4b6df39dbcffaa255825dc3
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-20 23:48:43 +0800

    Add task array and sorting function before rendering tasks to main

--- src/display-tasks.js
+++ src/display-tasks.js
@@ -1,96 +1,97 @@
 import deleteSvg from './del.svg';
 import editSvg from './edit.svg';
 import viewSvg from './view.svg';
-import { getNewTask } from './add-task';
+import { compareDesc } from "date-fns";
 
-class taskComponents {
-    main = document.querySelector("main");
+class renderProjectTasks {
+    taskContainer = document.querySelector(".tasks");
+    taskArr = [];
 
-    constructor(project, task){
-        this.project = project;
+    constructor(task){
         this.task = task;
     }
 
-    renderTitle = () => {
-        const projectTitle = document.createElement("h1");
-        const taskInitial = document.createElement("h2");
-        const newTaskDiv = document.createElement("div");
-        const createNewTask = document.createElement("button");
-    
-        projectTitle.textContent = this.project;
-        taskInitial.textContent = "Tasks";
-    
-        newTaskDiv.setAttribute("class", "new-task");
-        createNewTask.setAttribute("class", "new-task-button");
-        createNewTask.textContent = "Add Task";
-    
-        newTaskDiv.append(createNewTask);
-    
-        this.main.append(
-            projectTitle,
-            taskInitial,
-            newTaskDiv
-        );
+    sortTaskArray = (a, b) => {
+        if ( a.due < b.due ){
+            return -1;
+          }
+          if ( a.due > b.due ){
+            return 1;
+          }
+          return 0;
+    }
+
+    storeTasks = () => {
+        this.taskArr.push(this.task)
+        this.taskArr[0].sort( this.sortTaskArray );
     }
 
     renderTasks = () => {
+
         if (this.task[0] === ""){
             return
         }
-        else {
-        for (let tasks of this.task) {
-          const taskContainer = document.createElement("div");
-          const taskTitle = document.createElement("h3");
-          const taskDescription = document.createElement("p");
-          const taskDue = document.createElement("p");
-          const taskPriority = document.createElement("p");
 
-          const taskDoneBtn = document.createElement("button");
-          const taskViewBtn = document.createElement("button");
-          const taskEditBtn = document.createElement("button");
-          const taskDeleteBtn = document.createElement("button");
-          
-          const viewIcon = new Image();
-          const editIcon = new Image();
-          const deleteIcon = new Image();
-    
-            taskTitle.textContent = tasks.title;
-            taskDescription.textContent = "Description: " + tasks.desc;
-            taskDue.textContent = "Due Date: " + tasks.due;
-            taskPriority.textContent = "Priority: " + tasks.prio;
-            taskViewBtn.textContent = "View";
-            taskEditBtn.textContent = "Edit";
-            taskDeleteBtn.textContent = "Delete"
+        this.storeTasks();
+        this.taskContainer.innerHTML = "";
+
+        for (let i = 0; i < this.taskArr[0].length; i++) {
+            console.log(this.taskArr[0][i])
+        }
+
+            for (let tasks of this.taskArr[0]) {
+            const taskContainer = document.createElement("div");
+            const taskTitle = document.createElement("h3");
+            const taskDescription = document.createElement("p");
+            const taskDue = document.createElement("p");
+            const taskPriority = document.createElement("p");
+
+            const taskDoneBtn = document.createElement("button");
+            const taskViewBtn = document.createElement("button");
+            const taskEditBtn = document.createElement("button");
+            const taskDeleteBtn = document.createElement("button");
             
-            taskDoneBtn.setAttribute("class", "mark-done");
-            taskDoneBtn.textContent = "Mark as Done";
-    
-            taskViewBtn.setAttribute("class", "view-task");
-            viewIcon.src = viewSvg;
+            const viewIcon = new Image();
+            const editIcon = new Image();
+            const deleteIcon = new Image();
+        
+                taskTitle.textContent = tasks.title;
+                taskDescription.textContent = "Description: " + tasks.desc;
+                taskDue.textContent = "Due Date: " + tasks.due;
+                taskPriority.textContent = "Priority: " + tasks.prio;
+                taskViewBtn.textContent = "View";
+                taskEditBtn.textContent = "Edit";
+                taskDeleteBtn.textContent = "Delete"
+                
+                taskDoneBtn.setAttribute("class", "mark-done");
+                taskDoneBtn.textContent = "Mark as Done";
+        
+                taskViewBtn.setAttribute("class", "view-task");
+                viewIcon.src = viewSvg;
 
-            taskEditBtn.setAttribute("class", "edit-task");
-            editIcon.src = editSvg;
+                taskEditBtn.setAttribute("class", "edit-task");
+                editIcon.src = editSvg;
 
-            taskDeleteBtn.setAttribute("class", "delete-task");
-            deleteIcon.src = deleteSvg;
-    
-            taskViewBtn.append(viewIcon);
-            taskEditBtn.append(editIcon);
-            taskDeleteBtn.append(deleteIcon);
-    
-            taskContainer.append(
-                taskTitle,
-                taskDescription,
-                taskDue,
-                taskPriority,
-                taskDoneBtn,
-                taskViewBtn,
-                taskEditBtn,
-                taskDeleteBtn
-            );
-            this.main.append(taskContainer)
-        }} 
+                taskDeleteBtn.setAttribute("class", "delete-task");
+                deleteIcon.src = deleteSvg;
+        
+                taskViewBtn.append(viewIcon);
+                taskEditBtn.append(editIcon);
+                taskDeleteBtn.append(deleteIcon);
+        
+                taskContainer.append(
+                    taskTitle,
+                    taskDescription,
+                    taskDue,
+                    taskPriority,
+                    taskDoneBtn,
+                    taskViewBtn,
+                    taskEditBtn,
+                    taskDeleteBtn
+                );
+                this.taskContainer.append(taskContainer)
+        }
     }
 }
 
-export { taskComponents }
+export { renderProjectTasks }
