3a3e8920bf6dc73e3ca5dda1c5f330f7ffc53356
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-20 23:49:18 +0800

    Edit import target

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './main-content.css';
 import { landingProject } from './landing';
 import { sidebarModalFunction, sidebarProjectsEvent } from './sidebar-control';
 import { NewProjectFunction } from './add-project';
-import { renderProjectsToSidebar } from './display-projects';
+import { renderProjectsToSidebar } from './projects-to-sidebar';
 import { taskModalFunc } from './add-task';
 
 sidebarModalFunction();
