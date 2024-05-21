7f03c0736e87632eb926db76fec27f45c4da1b65
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-21 16:25:30 +0800

    Remove funcion that adds a new task button and transfer it to display-tasks module

--- src/display-projects.js
+++ src/display-projects.js
@@ -8,22 +8,13 @@ class renderProjectTitle {
   renderTitle = () => {
     const projectTitle = document.createElement("h1");
     const taskInitial = document.createElement("h2");
-    const newTaskDiv = document.createElement("div");
-    const createNewTask = document.createElement("button");
 
     projectTitle.textContent = this.project;
     taskInitial.textContent = "Tasks";
 
-    newTaskDiv.setAttribute("class", "new-task");
-    createNewTask.setAttribute("class", "new-task-button");
-    createNewTask.textContent = "Add Task";
-
-    newTaskDiv.append(createNewTask);
-
     this.main.append(
         projectTitle,
         taskInitial,
-        newTaskDiv
     );
   }
 }
