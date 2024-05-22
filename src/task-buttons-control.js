92aa1c8f886a58105c4e5e4699da2787ca1dbce0
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-22 13:46:46 +0800

    Add functionality to complete task and view task buttons

--- src/task-buttons-control.js
+++ src/task-buttons-control.js
@@ -1,21 +1,62 @@
+import { renderProjectTasks } from "./display-tasks";
+import { submitToStorage } from "./add-task";
+import { format } from "date-fns";
+
 const addEventToTaskButtons = () => {
     const markAsDoneBtns = document.querySelectorAll(".mark-done");
     const viewTask = document.querySelectorAll(".view-task");
+    const editTask = document.querySelectorAll(".edit-task");
 
     markAsDoneBtns.forEach((button) => {
-        button.addEventListener("click", markTaskDone)
+        button.addEventListener("click", completeTaskConfirmation)
     })
 
     viewTask.forEach((button) => {
         button.addEventListener("click", openViewTaskModal)
     })
 
+    editTask.forEach((button) => {
+        button.addEventListener("click", editTaskFunc)
+    })
 }
 
-const markTaskDone = (e) => {
-    const projectTitle = document.querySelector("main > h1").textContent
+const completeTaskConfirmation = (e) => {
     const parent = e.target.parentElement;
     const taskTitle = parent.querySelector("h3").textContent.toLowerCase();
+
+    const completeTaskModal = document.querySelector(".confirm-task-complete")
+    const completeTaskTitle = document.querySelector(".confirm-task-complete > h1")
+    completeTaskModal.showModal();
+    completeTaskTitle.textContent = taskTitle.toUpperCase();
+
+    const cancelComplete = document.querySelector("#cancel-complete");
+    cancelComplete.addEventListener("click", () => completeTaskModal.close())
+
+    const confirmComplete = document.querySelector("#confirm-complete");
+    confirmComplete.addEventListener("click", taskCompleted)
+
+}
+
+const taskCompleted = (e) => {
+    const projectTitle = document.querySelector("main > h1").textContent;
+    const completeTaskModal = document.querySelector(".confirm-task-complete")
+    const completedTaskTitle = e.target.parentElement.querySelector("h1").textContent.toLowerCase()
+
+    const projectInfo = JSON.parse(localStorage.getItem(projectTitle))
+        
+    for (let i = 0; i < projectInfo.tasks.length; i++) {
+        if (projectInfo.tasks[i].title === completedTaskTitle){
+            projectInfo.tasks = projectInfo.tasks.filter(item => item !== projectInfo.tasks[i])
+        }
+    }
+    
+    localStorage.setItem(projectTitle, JSON.stringify(projectInfo))
+
+    const updateTask = new renderProjectTasks(projectInfo.tasks)
+    
+    updateTask.renderTasks();
+
+    completeTaskModal.close();
 }
 
 const openViewTaskModal = (e) => {
@@ -40,6 +81,48 @@ const openViewTaskModal = (e) => {
     viewTaskModal.showModal();
 
     closeView.addEventListener("click", () => viewTaskModal.close())
+}
+
+const editTaskFunc = (e) => {
+    const editTaskModal = document.querySelector(".edit-task-modal");
+    const formatToday = format(new Date(), "yyyy-MM-dd")
+    
+
+    const parent = e.currentTarget.parentElement;
+
+    const taskToEdit = parent.querySelector("h3").textContent.toLowerCase();
+    const currentTaskDesc = parent.querySelector(":nth-child(2)").textContent.slice(13);
+    const currentTaskDue = parent.querySelector(":nth-child(3)").textContent.slice(10);
+    const currentTaskPrio = parent.classList.value;
+
+    const cancelEdit = document.querySelector("#edit-close");
+    const submitEdit = document.querySelector("#edit-submit");
+
+    const editTaskForm = document.querySelector(".edit-task-modal > form")
+    editTaskForm.querySelector("#due").setAttribute("min", formatToday)
+
+    editTaskForm.querySelector("#title").value = taskToEdit.toUpperCase();
+    editTaskForm.querySelector("#desc").value = currentTaskDesc;
+    editTaskForm.querySelector("#due").value = format(currentTaskDue, "yyyy-MM-dd");
+    editTaskForm.querySelector("#"+currentTaskPrio).checked = true;
+
+    editTaskModal.showModal();
+
+    cancelEdit.addEventListener("click", (e) => {
+        e.preventDefault
+        editTaskForm.reset();
+        editTaskModal.close();
+    })
+
+    submitEdit.addEventListener("click", confirmTaskEdit)
+}
+
+const confirmTaskEdit = (e) => {
+    const editTaskModal = document.querySelector(".edit-task-modal");
+
+    e.preventDefault();
+    
+    editTaskModal.close();
 }
 
 export { addEventToTaskButtons }
